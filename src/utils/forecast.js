c49cc7c996eb845c1e7b1ebe67eb3788eede2c99
sudipta.das@michelin.com <sudipta.das@michelin.com>
2019-05-06 09:36:43 +0530

    Added text to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined,'It is currently ' + body.currently.temperature + ' degrees out.')
+            callback(undefined,body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out.')
         }
    
     })
